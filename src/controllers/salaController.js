752e2492b2b3d4494243b5af61d958b0bb7e2367
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-08-23 19:55:26 -0300

    commit 23/08

--- src/controllers/salaController.js
+++ src/controllers/salaController.js
@@ -2,3 +2,52 @@ exports.get = async function () {
     const salaModel = require('../models/salaModel');
     return await salaModel.listarSalas();
 }
+
+exports.entrar=async (iduser,idsala)=>{
+    const sala = await salaModel.buscarSala(idsala);
+    let usuarioModel=require('../models/usuarioModel');
+    let user= await usuarioModel.buscarUsuario(iduser);
+    user.sala={_id:sala._id, nome:sala.nome, tipo:sala.tipo};
+    if(await usuarioModel.alterarUsuario(user)){
+        return {msg:"OK", timestamp:timestamp=Date.now()};
+    }
+    return false;
+}
+
+exports.sair = async (iduser, idsala) => {
+    let usuarioModel = require('../models/usuarioModel');
+    let user = await usuarioModel.buscarUsuario(iduser);
+
+    if (user && user.sala._id === idsala) {
+        user.sala = null; // Remover a sala associada ao usuário.
+        if (await usuarioModel.alterarUsuario(user)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+exports.enviarMensagem= async (nick, msg, idsala)=>{
+    const sala = await salaModel.buscarSala(idsala);
+    if(!sala.msgs){
+        sala.msgs=[];
+    }
+    timestamp=Date.now()
+    sala.msgs.push(
+        {
+            timestamp:timestamp,
+            msg:msg,
+            nick:nick
+        }
+    )
+    let resp = await salaModel.atualizarMensagens(sala);
+    return {"msg":"OK", "timestamp":timestamp};
+}
+
+exports.buscarMensagens = async (idsala, timestamp)=>{
+    let mensagens=await salaModel.buscarMensagens(idsala, timestamp);
+    return{
+        "timestamp":mensagens[mensagens.length - 1].timestamp,
+        "msgs":mensagens
+    };
+}
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,12 +1,33 @@
-const token = ("../util/token");
+
 const usuarioModel = require('../models/usuarioModel');
 
-exports.entrar=async(nick)=>{
-    let resp = await usuarioModel.registraUsuario(nick);
-    if(resp.insertId){
-        return { "idUser":resp.insertId,
-            "token": await token.setToken(JSON.stringify(resp.insertId).replace(/"/g, ''),nick),
-            "nick":nick}
+exports.entrar = async (nick) => {
+    let resp = await usuarioModel.registrarUsuario(nick);
+    const token = require("../util/token");
+    console.log(token)
+    console.log(resp)
+    if (resp.insertedId) {
+        console.log(JSON.stringify(resp.insertedId))
+        console.log(await token.setToken(JSON.stringify(resp.insertedId), nick));
+        return {
+            "idUser": resp.insertedId,
+            "token": await token.setToken(JSON.stringify(resp.insertedId).replace(/"/g, ''), nick),
+            "nick": nick
         }
     }
+    else{
+        return {
+            "status": "deu ruim"
+        }
+    }
+}
+
+exports.sair = async (idUser) => {
+    let user = await usuarioModel.buscarUsuario(idUser);
+    if (user) {
+        user.sala = null;  // Limpar a sala associada ao usuário.
+        await usuarioModel.alterarUsuario(user);  // Atualizar o usuário no banco.
+        return true;
+    }
+    return false;
 }
