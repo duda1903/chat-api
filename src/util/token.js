03f4594036f5ece36d78740d8e8b99a37408bb3c
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-08-29 17:48:32 -0300

    commit 23/08

--- src/util/token.js
+++ src/util/token.js
@@ -1,10 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const checktoken = async (token, id, key) => jwt.verify(token, key, (err, decoded) =>{
-    var jwt = require ('jsonwebtoken');
-    var token = jwt.sign({foo: 'bar'}, 'shhhhh');
-})
+const checkToken = async (token, id, key) => {
+    try {
+        // Verifica e decodifica o token
+        const decoded = jwt.verify(token, key);
 
+        // Verifica se o ID no token corresponde ao ID fornecido
+        if (decoded.id === id) {
+            return { valid: true, decoded };
+        } else {
+            return { valid: false, message: 'ID does not match' };
+        }
+    } catch (err) {
+        // Se houver um erro na verificação do token
+        return { valid: false, message: err.message };
+    }
+};
 const setToken = async (id, key) =>{
     console.log(id);
     if(id){
